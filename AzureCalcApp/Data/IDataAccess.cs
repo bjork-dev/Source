a34571faf07675b9706f19f3f0b0bbc5f5b5c354
Liam Björkman <liambjorkman00@gmail.com>
2021-10-13 15:35:12 +0200

    first commit

---
+++ AzureCalcApp/Data/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AzureCalcApp.Data
+{
+    interface IDataAccess
+    {
+        Task<HttpResponseMessage> PostAsync(string numbers);
+        Task<string> GetAsync();
+    }
+}
