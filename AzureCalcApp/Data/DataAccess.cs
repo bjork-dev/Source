067e5ea6cb1797ab1a7974d0643283e6607588f0
Liam Björkman <liambjorkman00@gmail.com>
2021-10-13 15:44:01 +0200

    Init

---
+++ AzureCalcApp/Data/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AzureCalcApp.Data
+{
+    public class DataAccess : IDataAccess
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public DataAccess(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string numbers)
+        {
+            var client = _httpClient.CreateClient();
+            var json = JsonSerializer.Serialize(numbers);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            return await client.PostAsync("", data);
+        }
+
+        public async Task<string> GetAsync()
+        {
+            var client = _httpClient.CreateClient();
+            var response = await client.GetAsync("");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ AzureCalcApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCalcApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string Result { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string result)
+        {
+            
+        }
+
+    }
+}
