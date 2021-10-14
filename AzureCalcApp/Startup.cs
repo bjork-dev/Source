9b8b7cfb9c3117afffe36c61f921c7c31954e8f0
Liam Björkman <Liam.Bjorkman@empirindustry.com>
2021-10-14 08:40:10 +0200

    Add application insights

--- AzureCalcApp/Startup.cs
+++ AzureCalcApp/Startup.cs
@@ -25,6 +25,7 @@ namespace AzureCalcApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(Configuration);
             services.AddRazorPages();
             services.AddSingleton<IDataAccess, DataAccess>();
         }
