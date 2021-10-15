db404a6807de049b9baf7d3921b4d5b5a33de528
Liam Björkman <liambjorkman00@gmail.com>
2021-10-15 10:02:01 +0200

    Update Calculation.cs

--- AzureCalcApp/Data/Calculation.cs
+++ AzureCalcApp/Data/Calculation.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace AzureCalcApp.Data
 {
-    public class Calculation
+    public class Calculation //Model from database
     {
         public string Id { get; set; }
         public string Result { get; set; }
